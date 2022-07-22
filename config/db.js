c307a44d8ec3e975a74b174ed0b9d5c7a7f8042d
DESKTOP-14NS0AA\Jony <jonyhern93@gmail.com>
2022-07-22 12:10:00 +0200

    pintar eventos con cors ok!

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '123412341234',
+    password: '12345',
     port: 3306,
     database: 'meuno'
 });
